9bc0a14df3b5ce64b87ba4f87e3b5804458cc7b7
cicciotazza <cicciotazza@hotmail.com>
2022-01-18 18:32:59 +0100

    3.3: React Components & the Virtual DOM
    Rev 1 following the example

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MainView from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
@@ -8,15 +9,13 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
     render() {
         return (
-            <div className="my-flix">
-                <div>Good morning</div>
-            </div>
+            <MainView />
         );
     }
 }
 
-// Find the root of myFlix app
+// Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tell React to render myFlix app in the root DOM element
+// Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);

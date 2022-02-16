c669b243118908d4aee521cbfec1e9062f1c11ba
cicciotazza <cicciotazza@hotmail.com>
2022-02-16 19:22:55 +0100

    3.8 upload v03

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,6 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
 import { devToolsEnhancer } from 'redux-devtools-extension';
-import MainView from './components/main-view/main-view';
 
 import './index.scss';
 

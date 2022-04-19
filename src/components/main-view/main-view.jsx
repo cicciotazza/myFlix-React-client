9ed97b058fe8d79fde1f12ced26cd080e9418cf7
cicciotazza <cicciotazza@hotmail.com>
2022-04-19 22:34:59 +0200

    3.8 v08 gh-page

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import axios from 'axios';
 import PropTypes from "prop-types";
 
-import { Button, Card, CardGroup, Col, Form, Container, Navbar, Nav, NavDropdown, Row } from 'react-bootstrap';
+import { Button, Card, CardGroup, Col, Form, Container, Nav, NavDropdown, Row } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 brach
 import './main-view.scss';
